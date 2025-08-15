72d4051537ff516a03bb4cb96fd2b4a7aa6a10e3
Sameer Dudeja <dsameer33@gmail.com>
2025-08-16 03:32:10 +0530

    Final fix

--- providers.tf
+++ providers.tf
@@ -1,11 +1,15 @@
 terraform {
+  required_version = ">= 1.5.7"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      # Pin to a modern provider that supports the LT fields used by the module
+      version = ">= 6.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
