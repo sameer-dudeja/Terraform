1ce6c81686137afd55706678941582569479bd96
Sameer Dudeja <dsameer33@gmail.com>
2025-08-16 02:20:19 +0530

    Autoscalling

--- main.tf
+++ main.tf
@@ -42,7 +42,28 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_arns = module.blog_alb.target_groups_arns
+
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = "Learning Terraform"
+  }
+}
+
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "alb-blog"
