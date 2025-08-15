11665d5088d2c5c888196df33570e4e8444cacc3
Sameer Dudeja <dsameer33@gmail.com>
2025-08-15 23:58:06 +0530

    Fix security group

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in"{
+resource "aws_security_group_rule" "blog_everything_out"{
   type = "egress"
   from_port = 0
   to_port = 0
