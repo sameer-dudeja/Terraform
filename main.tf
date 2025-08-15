e6bdaa4a2285f484164bd83b303358aa1769b147
Sameer Dudeja <dsameer33@gmail.com>
2025-08-16 02:30:32 +0530

    fix everything and rework

--- main.tf
+++ main.tf
@@ -23,93 +23,70 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id               = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-
-  target_group_arns = [module.blog_alb.target_groups["ex-instance"].arn]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "alb-blog"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-  }
+  name = "blog-alb"
 
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
-    Environment = "Development"
-    Project     = "Example"
+    Environment = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name = "blog"
+  version = "4.13.0"
 
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
