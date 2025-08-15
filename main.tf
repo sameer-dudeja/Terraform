479c92b178964da5f44d732bd29b4653c996bc6c
Sameer Dudeja <dsameer33@gmail.com>
2025-08-15 23:59:16 +0530

    fix

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog"{
   name = "blog"
-  description: "Allow hhtp and https in/ Allow everything out"
+  description = "Allow http and https in/ Allow everything out"
   
   vpc_id = data.aws_vpc.default.id
 
