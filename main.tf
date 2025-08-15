ac371f2311d10ff71adbe9c3097bb16e3ee37f43
Sameer Dudeja <dsameer33@gmail.com>
2025-08-16 01:45:12 +0530

    fix az issue

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west2a", "us-west2b", "us-west2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 
   tags = {
